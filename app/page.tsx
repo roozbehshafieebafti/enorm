76a5acd99c90dc267024687cf1ca63cd197dcf7c
personal <roozbehp.shafiee@gmail.com>
2023-09-16 16:33:58 +0300

    cars card completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+import { CarsCard } from "@/components/cars";
+
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="d-felx">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3 col-12 p-0"></div>
+          <div className="col-lg-9 col-12 p-0">
+            <CarsCard />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ components/cars/index.tsx
@@ -0,0 +1,108 @@
+import { FC } from "react";
+import Image from "next/image";
+
+import classes from "./styles/car.module.scss";
+import clsx from "clsx";
+import { Button } from "../button";
+
+export const CarsCard: FC = () => {
+  return (
+    <div className={clsx("card mt-5", classes.root)}>
+      <div className={classes.picture}>
+        <div>
+          <Image src="/images/car.png" layout="fill" alt={"car"} />
+        </div>
+      </div>
+      <div className={clsx("p-0", classes.information)}>
+        <div className="container">
+          <div className="row mb-3">
+            <span className="title-2 p-0">Humda</span>
+          </div>
+          <div className="row">
+            <div className="col-lg-4 col-6 mb-3 p-0">
+              <div>
+                <Image
+                  src="/images/milage.svg"
+                  width={16}
+                  height={12}
+                  alt="milage"
+                />
+                <span className={clsx("caption", classes.caption)}>Milage</span>
+              </div>
+              <div className={clsx("body-1", classes.data)}>69.426 km</div>
+            </div>
+            <div className="col-lg-4 col-6 p-0">
+              <div>
+                <Image
+                  src="/images/transmission.svg"
+                  width={16}
+                  height={12}
+                  alt="Transmission"
+                />
+                <span className={clsx("caption", classes.caption)}>
+                  Transmission
+                </span>
+              </div>
+              <div className={clsx("body-1", classes.data)}>69.426 km</div>
+            </div>
+            <div className="col-lg-4 col-6 p-0">
+              <div>
+                <Image
+                  src="/images/year.svg"
+                  width={16}
+                  height={12}
+                  alt="year"
+                />
+                <span className={clsx("caption", classes.caption)}>Year</span>
+              </div>
+              <div className={clsx("body-1", classes.data)}>69.426 km</div>
+            </div>
+            <div className="col-lg-4 col-6 p-0">
+              <div>
+                <Image
+                  src="/images/intake.svg"
+                  width={16}
+                  height={12}
+                  alt="intake"
+                />
+                <span className={clsx("caption", classes.caption)}>Intake</span>
+              </div>
+              <div className={clsx("body-1", classes.data)}>69.426 km</div>
+            </div>
+            <div className={clsx("col-lg-4 col-6 p-0", classes.margin)}>
+              <div style={{ whiteSpace: "nowrap" }}>
+                <Image
+                  src="/images/power.svg"
+                  width={16}
+                  height={12}
+                  alt="milage"
+                />
+                <span className={clsx("caption", classes.caption)}>
+                  Power KW (PK)
+                </span>
+              </div>
+              <div className={clsx("body-1", classes.data)}>69.426 km</div>
+            </div>
+            <div className={clsx("col-lg-4 col-6 p-0", classes.margin)}>
+              <div>
+                <Image
+                  src="/images/color.svg"
+                  width={16}
+                  height={12}
+                  alt="color"
+                />
+                <span className={clsx("caption", classes.caption)}>Color</span>
+              </div>
+              <div className={clsx("body-1", classes.data)}>69.426 km</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="p-0 d-flex justify-content-end align-items-end mt-4">
+        <Button shape="secondary" style={{ whiteSpace: "nowrap" }}>
+          View car
+        </Button>
+      </div>
+    </div>
+  );
+};
